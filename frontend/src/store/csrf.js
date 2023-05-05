7d13ae0f4335dc0472a70cd2de3e98702d3d2b2a
snydernb1 <snydernb1@gmail.com>
2023-05-05 08:16:53 -0600

    Created signin thunk and dispatch

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -16,10 +16,10 @@ export async function csrfFetch(url, options = {}) {
     options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
   }
   // call the default window's fetch with the url and the options passed in
-  console.log('is csrf fetch running?')
-  console.log(url, options)
+  // console.log('is csrf fetch running?')
+  // console.log(url, options)
   const res = await window.fetch(url, options);
-  console.log(res)
+  // console.log(res)
 
   // if the response status code is 400 or above, then throw an error with the
     // error being the response
